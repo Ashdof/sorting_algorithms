00bf1ba514be53db03453dbde9cc52b0dd975e0f
Ashdof <mzigah4@gmail.com>
2023-01-25 10:29:43 +0000

    Minor fix

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -20,14 +20,14 @@ void shell_sort(int *array, size_t size)
 		return;
 
 	while (gap < size / 3)
-		gap *= 3 + 1;
+		gap = gap * 3 + 1;
 
 	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
-			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			for (j = i; j >= gap && array[j - gap] > temp; j = j - gap)
 				array[j] = array[j - gap];
 			array[j] = temp;
 		}
