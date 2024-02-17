7ef166fcdf452abe5c0ec29c5458acca72ff2a89
Ashdof <mzigah4@gmail.com>
2024-02-17 15:13:16 +0000

    Task 6 - a function that sorts an array of integers in ascending order using the Counting sort algorithm

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void selection_sort(int *, size_t);
 void quick_sort(int *, size_t);
 void shell_sort(int *, size_t);
 void cocktail_sort_list(listint_t **);
+void counting_sort(int *, size_t);
 
 #endif /* SORT_H */
